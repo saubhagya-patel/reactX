c5e085496de9f5519ae8599370c63ea1c7da4672
Saubhagya Patel <patelsaubhagya0144@gmail.com>
2024-07-09 21:22:50 +0530

    formatting and design changes

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
-const startGameButton=document.getElementById('startGameButton');
-const gameBox=document.getElementById('game-box');
-const entryBox=document.getElementById('entry-box');
-const resultBox=document.getElementById('result-box');
-const messageBox=document.getElementById('message-box');
+const startGameButton = document.getElementById('startGameButton');
+const gameBox = document.getElementById('game-box');
+const entryBox = document.getElementById('entry-box');
+const resultBox = document.getElementById('result-box');
+const messageBox = document.getElementById('message-box');
 const countdownElement = document.getElementById('timer-box');
-const body=document.getElementById('test');
-const replayButtonContainer=document.getElementById('replayButtonContainer');
+const body = document.getElementById('test');
+const replayButtonContainer = document.getElementById('replayButtonContainer');
 let difficulty = document.getElementById('difficulty').value;
 let iterations = document.getElementById('iterations').value;
-const intermediateReactionTime=[];
-const intermediateHeights=[];
-const intermediateWidths=[];
-const intermediateDistances=[];
-const intermediateSpeeds=[];
-const bgColor=["#56b4e9","#42ffa7","#242124"]
-const circleColor=["#e69f00","a3f742","#1f1f1f"]
-const difficultyArray=["70px","50px","30px"];
-const circle=document.getElementById('circle')
+const intermediateReactionTime = [];
+const intermediateHeights = [];
+const intermediateWidths = [];
+const intermediateDistances = [];
+const intermediateSpeeds = [];
+const bgColor = ["#56b4e9", "#42ffa7", "#242124"]
+const circleColor = ["#e69f00", "a3f742", "#1f1f1f"]
+const difficultyArray = ["70px", "50px", "30px"];
+const circle = document.getElementById('circle')
 
-startGameButton.addEventListener('click', function() {
+startGameButton.addEventListener('click', function () {
     difficulty = document.getElementById('difficulty').value;
     iterations = document.getElementById('iterations').value;
-    if(!iterations) {
-        iterations=1;
+    if (!iterations) {
+        iterations = 1;
     }
     entryBox.remove();
     messageBox.remove();
-    countdown(0);
+    countdown(3);
 });
 
 function countdown(seconds) {
     let remainingSeconds = seconds;
     countdownElement.innerHTML = `<p class="lead">The countdown begins be ready...</p>${remainingSeconds}`;
 
-    const intervalId = setInterval(function() {
+    const intervalId = setInterval(function () {
         remainingSeconds--;
         countdownElement.innerHTML = `<p class="lead">The countdown begins be ready...</p>${remainingSeconds}`;
         if (remainingSeconds <= 0) {
@@ -45,64 +45,64 @@ function countdown(seconds) {
 }
 
 function playBeep() {
-    let audio=document.getElementById('audio-player');
+    let audio = document.getElementById('audio-player');
     audio.play();
 }
 function calculateDistance(x1, y1, x2, y2) {
-    let xDistance=(x1-x2)**2;
-    let yDistance=(y1-y2)**2;
-    let distance = (xDistance+yDistance)**(1/2);
+    let xDistance = (x1 - x2) ** 2;
+    let yDistance = (y1 - y2) ** 2;
+    let distance = (xDistance + yDistance) ** (1 / 2);
     let roundedDistance = Math.round(distance * 100) / 100;
     intermediateDistances.push(roundedDistance);
 }
 
 function startGame() {
     countdownElement.remove();
-    body.style.backgroundColor=bgColor[difficulty];
-    circle.style.backgroundColor=circleColor[difficulty];
-    gameBox.style.display='block';
-    circle.style.height=difficultyArray[difficulty];
+    body.style.backgroundColor = bgColor[difficulty];
+    circle.style.backgroundColor = circleColor[difficulty];
+    gameBox.style.display = 'block';
+    circle.style.height = difficultyArray[difficulty];
     gameLogic();
 }
 
 function generateRandomNumber(start, end) {
-    const min=start;
-    const max=end;
-    let randomNumber = Math.floor(Math.random()*(max-min+1)) + min;
+    const min = start;
+    const max = end;
+    let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
     return randomNumber;
 }
 
 let startTime;
 function makeDivAtRandomLocation() {
-    let maxWidth=window.innerWidth;
-    let maxHeight=window.innerHeight;
-    let newHeight=generateRandomNumber(1,maxHeight);
-    let newWidth=generateRandomNumber(1,maxWidth);
+    let maxWidth = window.innerWidth;
+    let maxHeight = window.innerHeight;
+    let newHeight = generateRandomNumber(1, maxHeight);
+    let newWidth = generateRandomNumber(1, maxWidth);
     intermediateHeights.push(newHeight);
     intermediateWidths.push(newWidth);
-    circle.style.top=newHeight+"px";
-    circle.style.left=newWidth+"px";
-    startTime=Date.now();
+    circle.style.top = newHeight + "px";
+    circle.style.left = newWidth + "px";
+    startTime = Date.now();
 }
 
 function returnAverageOfArray(arr) {
-    let sm=0;
+    let sm = 0;
     arr.forEach(element => {
-        sm+=element;
+        sm += element;
     });
-    let avg= (sm/arr.length);
+    let avg = (sm / arr.length);
     let roundedAvg = Math.round(avg * 100) / 100;
     return roundedAvg;
 }
 
-circle.addEventListener("click", ()=> {
-    let endTime=Date.now();
-    let elapsedTime=endTime-startTime;
-    intermediateReactionTime.push(elapsedTime/1000)
+circle.addEventListener("click", () => {
+    let endTime = Date.now();
+    let elapsedTime = endTime - startTime;
+    intermediateReactionTime.push(elapsedTime / 1000)
 })
 
 async function gameLogic() {
-    for(let i=0; i<iterations; i++) {
+    for (let i = 0; i < iterations; i++) {
         makeDivAtRandomLocation();
         await waitForClick();
     }
@@ -113,24 +113,24 @@ async function gameLogic() {
 
 function waitForClick() {
     return new Promise(resolve => {
-        circle.addEventListener("click",()=> {   
+        circle.addEventListener("click", () => {
             resolve();
-        },{once:true});
+        }, { once: true });
     });
 }
 
 function calculateIntermediateDistances() {
-    let initialWidth=startGameButton.offsetLeft;
-    let intialHeight=startGameButton.offsetTop;
+    let initialWidth = startGameButton.offsetLeft;
+    let intialHeight = startGameButton.offsetTop;
     calculateDistance(intialHeight, initialWidth, intermediateHeights[0], intermediateWidths[0]);
-    for(let i=1; i<iterations; i++) {
-        calculateDistance(intermediateHeights[i-1], intermediateWidths[i-1], intermediateHeights[i], intermediateWidths[i]);
+    for (let i = 1; i < iterations; i++) {
+        calculateDistance(intermediateHeights[i - 1], intermediateWidths[i - 1], intermediateHeights[i], intermediateWidths[i]);
     }
 }
 
 function calculateIntermediateSpeeds() {
-    for(let i=0; i<intermediateDistances.length; i++) {
-        let speed=intermediateDistances[i]/intermediateReactionTime[i];
+    for (let i = 0; i < intermediateDistances.length; i++) {
+        let speed = intermediateDistances[i] / intermediateReactionTime[i];
         let roundedSpeed = Math.round(speed * 100) / 100;
         intermediateSpeeds.push(roundedSpeed);
     }
@@ -138,9 +138,9 @@ function calculateIntermediateSpeeds() {
 
 function endGame() {
     gameBox.remove();
-    body.style.backgroundColor='';
-    circle.style.backgroundColor='';
-    resultBox.style.display='block';
+    body.style.backgroundColor = '';
+    circle.style.backgroundColor = '';
+    resultBox.style.display = 'block';
     showResult();
     showAverage();
 }
@@ -149,16 +149,16 @@ function showResult() {
     let table = document.createElement('table');
     let headerRow = table.createTHead().insertRow();
     headerRow.innerHTML = '<th>Intermediate Distances(pixels)</th><th>Intermediate Reaction Times(sec)</th><th>Intermediate Speeds(pixels/sec)</th>'
-    let tbody=document.createElement('tbody');
+    let tbody = document.createElement('tbody');
     table.appendChild(tbody);
 
     for (let i = 0; i < iterations; i++) {
-        let row = tbody.insertRow(); 
+        let row = tbody.insertRow();
 
         let cell1 = row.insertCell();
         let cell2 = row.insertCell();
         let cell3 = row.insertCell();
-        
+
         cell1.textContent = intermediateDistances[i];
         cell2.textContent = intermediateReactionTime[i];
         cell3.textContent = intermediateSpeeds[i];
@@ -173,16 +173,16 @@ function showAverage() {
     let table = document.createElement('table');
     let headerRow = table.createTHead().insertRow();
     headerRow.innerHTML = '<th>Average Distance(pixels)</th><th>Average Reaction Time(sec)</th><th>Average Speed(pixels/sec)</th>'
-    let tbody=document.createElement('tbody');
+    let tbody = document.createElement('tbody');
     table.appendChild(tbody);
-    let row=tbody.insertRow();
-    let cell1=row.insertCell();
-    let cell2=row.insertCell();
-    let cell3=row.insertCell();
+    let row = tbody.insertRow();
+    let cell1 = row.insertCell();
+    let cell2 = row.insertCell();
+    let cell3 = row.insertCell();
 
     let avgDistance = returnAverageOfArray(intermediateDistances);
     let avgTime = returnAverageOfArray(intermediateReactionTime);
-    let avgSpeed=avgDistance/avgTime;
+    let avgSpeed = avgDistance / avgTime;
     let roundedSpeed = Math.round(avgSpeed * 100) / 100;
 
     cell1.textContent = avgDistance;
@@ -192,5 +192,5 @@ function showAverage() {
     table.classList.add('table');
     table.classList.add('table-striped');
     table.classList.add('table-bordered');
-    document.getElementById('avgTableContainer').appendChild(table);   
+    document.getElementById('avgTableContainer').appendChild(table);
 }
